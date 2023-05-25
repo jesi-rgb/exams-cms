8a3b2a74955a82b07ce5b79860465e931e179c99
Jesús Enrique Rascón <blograso@gmail.com>
2023-05-25 10:21:24 +0200

    subtipo1 tema number -> string

--- schemas/subtipo1.js
+++ schemas/subtipo1.js
@@ -5,7 +5,7 @@ export default {
   fields: [
     { name: 'name', type: 'string', title: 'Título (no se usa en la aplicación)' },
     { title: 'Bloque', name: 'bloque', type: 'string' },
-    { title: 'Tema', name: 'tema', type: 'number' },
+    { title: 'Tema', name: 'tema', type: 'string' },
     {
       title: 'Enunciado',
       name: 'enunciado',
